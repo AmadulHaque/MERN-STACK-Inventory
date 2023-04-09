77fc1f424763258fcf6f55c4ee8ec8fdd8a4b412
AmadulHaque <92516695+AmadulHaque@users.noreply.github.com>
2023-04-09 23:37:36 +0600

    asdfasdf
    asdfasdf

--- src/Services/common/DetailsByIDService.js
+++ src/Services/common/DetailsByIDService.js
@@ -3,19 +3,16 @@ const DetailsByIDService= async (Request,DataModel) => {
     try{
 
         let DetailsID=Request.params.id;
-        const userEmail = Request.headers['email'];
-
         let UserEmail=Request.headers['email'];
 
-        const ObjectId = mongoose.Types.ObjectId;
         let QueryObject={};
         QueryObject['_id']=new mongoose.Types.ObjectId(DetailsID);
         QueryObject['UserEmail']=UserEmail;
 
-        let data = await DataModel.aggregate([
-            {$match:{_id:DetailsID}}
-        ])
 
+        let data = await DataModel.aggregate([
+            {$match: QueryObject}
+        ])
         return {status: "success", data: data}
     }
     catch (error) {
